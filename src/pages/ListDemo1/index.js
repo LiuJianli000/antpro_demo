3294b79a2a8d4d6ec669d78c729f888c08019bd3
liujianli <376122478@qq.com>
2020-05-19 10:51:41 +0800

    基础表格

--- src/pages/ListDemo1/index.js
+++ src/pages/ListDemo1/index.js
@@ -24,16 +24,92 @@ const initColumns = [
 class ListDemo1 extends Component {
   state = {
     columns: [],  // 渲染到 Table 的列，初始为空
+    page: 1,
+    pageSize: 10,
+    total: [],
     data: [
       {
         id: 1,
         name: 'liu',
         age: 23,
         sex: 'man'
-      }
+      },
+      {
+        id: 1,
+        name: 'liu',
+        age: 23,
+        sex: 'man'
+      },
+      {
+        id: 1,
+        name: 'liu',
+        age: 23,
+        sex: 'man'
+      },
+      {
+        id: 1,
+        name: 'liu',
+        age: 23,
+        sex: 'man'
+      },
+      {
+        id: 1,
+        name: 'liu',
+        age: 23,
+        sex: 'man'
+      },
+      {
+        id: 1,
+        name: 'liu',
+        age: 23,
+        sex: 'man'
+      },
+      {
+        id: 1,
+        name: 'liu',
+        age: 23,
+        sex: 'man'
+      },
+      {
+        id: 1,
+        name: 'liu',
+        age: 23,
+        sex: 'man'
+      },
+      {
+        id: 1,
+        name: 'liu',
+        age: 23,
+        sex: 'man'
+      },
+      {
+        id: 1,
+        name: 'liu',
+        age: 23,
+        sex: 'man'
+      },
+      {
+        id: 1,
+        name: 'liu',
+        age: 23,
+        sex: 'man'
+      },
+      {
+        id: 1,
+        name: 'liu',
+        age: 23,
+        sex: 'man'
+      },
+      {
+        id: 1,
+        name: 'liu',
+        age: 23,
+        sex: 'man'
+      },
     ]
   }
 
+  // 选择列
   handleColChange = (val) => {
     const columns = []
 
@@ -50,8 +126,17 @@ class ListDemo1 extends Component {
     })
   }
 
+  // 表格参数变化
+  handleTableChange = (pagination) => {
+    const { current, pageSize } = pagination
+    this.setState({
+      page: current,
+      pageSize,
+    })
+  }
+
   render() {
-    const { data, columns } = this.state
+    const { data, columns, total, page, pageSize } = this.state
     return (
       <Card>
         <div style={{ textAlign: 'right', marginBottom: '5px' }}>
@@ -62,6 +147,20 @@ class ListDemo1 extends Component {
           rowKey='id'
           columns={columns}
           dataSource={data}
+          onChange={this.handleTableChange}
+          bordered
+          style={{ marginTop: 20 }}
+          pagination={{
+            total,
+            current: page,
+            pageSize,
+            pageSizeOptions: ['5', '10', '15', '20'],
+            showSizeChanger: true,
+            showQuickJumper: true,
+            showTotal: totals => (
+              <p style={{ position: 'absolute', left: 0 }}>总共有 {totals} 条记录</p>
+            ),
+          }}
         />
       </Card>
     )
