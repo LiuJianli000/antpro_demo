8b810be409758f6b92a836ede4a743fe2b0936ae
LiuJianli000 <376122478@qq.com>
2020-09-18 14:51:10 +0800

    aaa

---
+++ src/pages/DragElement/aaa.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class DragElement2 extends Component {
+  state = {}
+
+  render() {
+    return (
+      <>
+        hello
+      </>
+    )
+  }
+}
+
+export default DragElement2
